37930bb60cea99a833b479039a55bb2f5fec192b
Ika Javakhishvili <ikajava@gmail.com>
2018-02-18 13:38:42 +0400

    First lesson

--- Day1_Introduction/MyPlayground.playground/Contents.swift
+++ Day1_Introduction/MyPlayground.playground/Contents.swift
@@ -2,11 +2,64 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// ცვლადის ცხადი დეკლარირება
+var secondPersonName: String
 
-print(str)
+//ცვლადის ცხადი დეკლარირება ინიციალიზაციით
+var thirdPersonName = String()
 
-str
+var myAge = Int()               // მთელი რიცხვის დეკლარირება
+var acceleration: Decimal       // ნამდვილი, მაღალი სიზუსტის რიცხვის დეკლარირება
+acceleration = 9.8
 
-var age = 34
+
+secondPersonName = "Davit"
+var personFirstName = "Nikoloz"
+var personSurname = "Lartsuliani"
+myAge = 30
+var myAgeInString = "30"
+Int(myAgeInString)              // მონაცემის ტიპის შეცვლა
+print("\(personFirstName) \(personSurname) is \(myAge) years old") //სხვადასხვა ტიპის მონაცემების String-ად ინტერპოლაცია
+
+var anotherInt: UInt            //Unsigned Int - მთელი დადებითი რიცხვის დეკლარირება
+var doubleNumber: Double
+var anotherDouble = Double()
+
+let number1 = 100
+var number2 = 6
+
+number2 = number2 + 1
+number2 += 2
+number2 -= 3
+
+
+number1 >= number2
+number2 > number1
+number2 <= number1
+number2 == number1
+number2 != number1
+
+print(number1%number2)
+
+//acceleration = 10
+
+personFirstName = "Bye, \nPlayground"
+
+print(personFirstName)
+print(personFirstName)
+
+
+
+
+if !(personFirstName == "Nikoloz" || personSurname == "Lartsuliani")  {
+    let welcomeMessage = "Hi there"
+    if 1 > 3 {
+        print(welcomeMessage)
+    }
+    print(welcomeMessage)
+}
+
+
+let (x,y,z) = (10,"11",20)
+
 
